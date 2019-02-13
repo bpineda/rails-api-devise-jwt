428906ef8e856fc2e8bc8afd0966f1c09bf51c86
Bernard Pineda <_@bernardopineda.mx>
2019-02-12 18:27:14 -0800

    Set devise to use custom session controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users, defaults: { format: :json }
+  devise_for :users, defaults: { format: :json }, 
+    controllers: {
+      sessions: 'users/sessions'
+    }
   get 'destinations', to: 'destinations#index'
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
